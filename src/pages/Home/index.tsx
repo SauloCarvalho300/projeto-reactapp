252198ed7188a8975cdb143a72da5f69c4a8282f
Saulo Carvalho <saulo_carvalho123@hotmail.com>
2024-08-27 20:47:09 -0300

    Projeto finalizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './styles.css'
 
 import { Card, CardProps } from '../../components/Card'
 
+interface UserResponse {
+  avatar_url: string
+  name: string
+}
+
+interface User {
+  avatarUrl: string
+  name: string
+}
+
+
 export default function Home() {
+  const [user, setUser] = useState<User>()
   const [studentName, setStudentName] = useState<string>('')
   const [students, setStudents] = useState<CardProps[]>([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://api.github.com/users/SauloCarvalho300")
+      const data = await response.json() as UserResponse
+
+      setUser({
+        avatarUrl: data.avatar_url,
+        name: data.name
+      })
+      
+    }
+    fetchData()
+  }, [])
 
   function handleAddStudent() {
   if (studentName === "") return alert('O nome não pode estar vazio.')
@@ -29,8 +55,8 @@ export default function Home() {
         <h1>Lista de presença</h1>
 
         <div>
-          <strong>Saulo Carvalho</strong>
-          <img src="https://github.com/SauloCarvalho300.png" alt="foto do perfil" />
+          <strong>{user?.name}</strong>
+          <img src={user?.avatarUrl} alt="foto do perfil" />
         </div>
       </header>
 
